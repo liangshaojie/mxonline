4009362162984132b5de0274f27ad4d9faec143d
liangshaojie <877942720@qq.com>
2018-05-12 22:11:07 +0800

    登录验证

---
+++ apps/users/forms.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+__author__ = "lsj"
+__date__ = '2018/5/12/012 21:45'
+
+from django import forms
+
+# from captcha.fields import CaptchaField
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True)
+
+
+# class RegisterForm(forms.Form):
+#     email = forms.EmailField(required=True)
+#     password = forms.CharField(required=True, min_length=4)
+#     captcha = CaptchaField()
+#
+#
+# class ForgetPwForm(forms.Form):
+#     email = forms.EmailField(required=True)
+#     captcha = CaptchaField()
+#
+#
+# class ResetForm(forms.Form):
+#     email = forms.EmailField(required=True)
+#     password = forms.CharField(required=True, min_length=4)
+#     password2 = forms.CharField(required=True, min_length=4)
--- apps/users/views.py
+++ apps/users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate,login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from .models import UserProfile
+from django.views.generic.base import View
+from .forms import LoginForm
 
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
@@ -14,15 +16,22 @@ class CustomBackend(ModelBackend):
         except Exception as e:
             return None
 
-def user_login(request):
-    if request.method == "POST":
-        user_name = request.POST.get("username","")
-        pass_word = request.POST.get("password","")
-        user = authenticate(username = user_name,password = pass_word)
-        if user is not None:
-            login(request,user)
-            return render(request,"index.html",)
+class LoginView(View):
+
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self,request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get("username", "")
+            pass_word = request.POST.get("password", "")
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return render(request, "index.html", )
+            else:
+                return render(request, "login.html", {"msg": "用户名或者密码错误"})
         else:
-            return render(request,"login.html",{"msg":"用户名或者密码错误"})
-    elif request.method == "GET":
-        return render(request,"login.html",{})
+            return render(request, 'login.html', {'login_form': login_form})
+
